b33891ec55c005493a215cffc5fcf99a63eb51fe
stho32 <stho32github@gmail.com>
2022-01-15 17:03:46 +0100

    ... did not work out as expected

--- README.md
+++ README.md
@@ -8,10 +8,21 @@ https://github.com/stho32/Collection-Of-Challenges/blob/master/Common-Requiremen
 - [ ] (R001) Find yourself a list of jokes or something like this on the internet. Do not forget to document the source. 
 
 Jokes: https://github.com/yesinteractive/dadjokes/blob/master/controllers/jokes.txt
-Blaques en francais : https://perso.crans.org/besson/bin/.blagues.txt
+
 
 - [ ] (R002) Write a simple algorithm that selects one joke/saying randomly.
 - [ ] (R003) The joke or saying should be converted to speech so you can listen to it instead of having to read it.
 
+
+
+https://www.comment-economiser.fr/blagues-courtes-droles.html
+
+C'est l'histoire du ptit dej, tu la connais ? - Pas de bol.
+
+C'est l'histoire d'une blague vaseuse - Mets tes bottes.
+
+Que demande un footballeur à son coiffeur ? - La coupe du monde s’il vous plait
+
+C'est l'histoire d'un pingouin qui respire par les fesses - Un jour il s’assoit et il meurt.
 
 
--- Source/jokey/jokey.Console/Program.cs
+++ Source/jokey/jokey.Console/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+//var blagues = File.ReadAllLines(args[0]);
+//var random = new Random();
+//var numero = random.Next(0, blagues.Length);
+//var blague = blagues[numero];
+
+var blague = @"
+Un jour, trois hommes partent en excursion, lorsque soudainement ils
+arrivent a une enorme riviere d‚chainee.
+Il fallait absolument qu'ils se rendent de l'autre cote, mais
+n'avaient aucune idee de la façon de la traverser.
+Un des hommes pria Dieu en disant : ""SVP mon Dieu, donne-moi la
+    force de traverser la riviere.""
+POUF, Dieu lui donna de gros bras et des jambes tres puissantes,
+    et il fut capable de traverser la riviere a la nage en 2 heures,
+mais
+    faillit se noyer a deux reprises.
+    Voyant cela, le deuxieme homme pria lui aussi Dieu : ""SVP mon Dieu,
+donne-moi la force ... et les outils pour traverser la riviere.""
+ 
+POUF, Dieu lui donna une chaloupe et des avirons, et il put
+traverser la rivière en 1 heure, mais faillit chavirer a deux
+reprises.
+    Le troisieme homme fort de l'experience de ses deux compagnons,
+    decide lui aussi de prier Dieu : ""SVP mon Dieu, donne-moi la force,
+les
+    outils et l'intelligence pour traverser la riviere.""
+POUF, Dieu le changea en femme. Elle verifia la carte, marcha
+environ 200 metres en amont et traversa le pont.
+ 
+    Envoyez ceci a une femme intelligente qui a besoin de rire et
+aux hommes qui sont capables de s'en amuser meme si la realite est
+    dure";
+
+var processStartInfo = new ProcessStartInfo("espeak");
+processStartInfo.ArgumentList.Add("-vfr");
+processStartInfo.ArgumentList.Add(blague);
+Console.WriteLine(blague);
+Process.Start(processStartInfo)?.WaitForExit();
